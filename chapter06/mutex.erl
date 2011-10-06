3cb91fddbf8b651fae573ef1aa0bef84375fbfc4
秋間 武志 <akima@akima-no-MacBook-Air.local>
2011-10-06 13:04:49 +0900

    ２つのプロセスが一つのmutexをとろうとして、取得した片方のプロセスが異常終了した場合、もう片方がmutexの解放待ちになってしまって固まることを確認しました
    19> c(mutex).
    {ok,mutex}
    20> mutex:start().
    true
    21> user_start(user1).
    ** exception error: undefined shell command user_start/1
    22> mutex:user_start(user1).
    true
    23> mutex:user_start(user2).
    true
    24> mutex:show().
    mutex belongs to no oneshow
    25> mutex:user_send(user1, work).
    work
    26> mutex:show().                
    mutex belongs to <0.83.0>show
    27> self().
    <0.81.0>
    28> mutex:user_send(user2, work).
    work
    29> self().                      
    <0.81.0>
    30> mutex:show().                
    mutex belongs to <0.83.0>show
    31> mutex:user_send(user1, error).
    error
    32> 
    =ERROR REPORT==== 6-Oct-2011::13:02:12 ===
    Error in process <0.83.0> with exit value: {badarith,[{mutex,add_one,1}]}
    
    
    BREAK: (a)bort (c)ontinue (p)roc info (i)nfo (l)oaded
           (v)ersion (k)ill (D)b-tables (d)istribution
    a

--- chapter06/mutex.erl
+++ chapter06/mutex.erl
@@ -2,6 +2,8 @@
 -export([start/0, stop/0, show/0]).
 -export([wait/0, signal/0]).
 -export([init/0]).
+-export([user_start/1, user_send/2]).
+-export([user_loop/1]).
 
 start() -> register(mutex, spawn(?MODULE, init, [])).
 stop()  -> mutex ! stop.
@@ -37,3 +39,34 @@ terminate() ->
   end.
 
 
+user_start(Name) ->
+  register(Name, spawn(?MODULE, user_loop, [Name])).
+
+user_send(Name, Msg) -> Name ! Msg.
+
+user_loop(Name) ->
+  receive
+    work -> 
+      mutex:wait(),
+      user_loop(Name);
+    stop -> 
+      ok;
+    error ->
+      add_one(one)
+  end.
+
+add_one(Int) -> Int + 1.
+
+
+% mutex:start()..
+% mutex:user_start(user1).
+% mutex:user_start(user2).
+% mutex:show().
+% mutex:user_send(user1, work).
+% mutex:show().
+% mutex:user_send(user2, work).
+% mutex:show().
+% mutex:user_send(user1, error).
+% mutex:show().
+% mutex:user_send(user2, stop).
+% mutex:show().
