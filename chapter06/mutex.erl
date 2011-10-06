41413f9a729e3340711359bc6b221d71b0917422
秋間 武志 <akima@akima-no-MacBook-Air.local>
2011-10-06 12:53:59 +0900

    mutex:show() を追加してmutexを持っているプロセスを確認できるようにしました

--- chapter06/mutex.erl
+++ chapter06/mutex.erl
@@ -1,10 +1,11 @@
 -module(mutex).
--export([start/0, stop/0]).
+-export([start/0, stop/0, show/0]).
 -export([wait/0, signal/0]).
 -export([init/0]).
 
 start() -> register(mutex, spawn(?MODULE, init, [])).
 stop()  -> mutex ! stop.
+show()  -> mutex ! show.
 
 wait() ->
   mutex ! {wait, self()},
@@ -17,18 +18,22 @@ init() -> free().
 
 free() ->
   receive
+    show -> io:format("mutex belongs to no one", []), free();
     {wait, Pid} -> Pid ! ok, busy(Pid);
     stop -> terminate()
   end.
 
 busy(Pid) ->
   receive
+    show -> io:format("mutex belongs to ~w", [Pid]), busy(Pid);
     {signal, Pid} -> free()
   end.
 
 terminate() ->
   receive
+    show -> io:format("mutex belongs to no one and now terminating", []), terminate();
     {wait, Pid} -> exit(Pid, kill), terminate()
     after 0 -> ok
   end.
+
 
