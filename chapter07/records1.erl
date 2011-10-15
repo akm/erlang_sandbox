2807cce2615c68114459efd22e6ab9f111cdbf55
akima <akima@nautilus-technologies.com>
2011-10-16 00:12:46 +0900

    exercise7-1をやりました

--- chapter07/records1.erl
+++ chapter07/records1.erl
@@ -9,7 +9,7 @@
 -module(records1).
 -export([birthday/1, joe/0, showPerson/1]).
 
--record(person, {name,age=0,phone}).
+-record(person, {name,age=0,phone,address}).
 
 birthday(#person{age=Age} = P) ->
   P#person{age=Age+1}.
@@ -21,5 +21,3 @@ joe() ->
 
 showPerson(#person{age=Age,phone=Phone,name=Name}) ->
   io:format("name: ~p  age: ~p  phone: ~p~n", [Name,Age,Phone]).
-
-
