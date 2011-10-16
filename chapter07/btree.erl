66c16a2932e53fb82704d9e75bb0d120d6feec58
akima <akima@nautilus-technologies.com>
2011-10-16 16:46:23 +0900

    exercise7-5 二分探索木の実装完了

--- chapter07/btree.erl
+++ chapter07/btree.erl
@@ -3,6 +3,7 @@
 -export([flatten/1]).
 -export([sum/1, max/1, min/1]).
 -export([is_ordered/1]).
+-export([add/2]).
 -include("btree.hrl").
 
 new(Left, Data, Right) ->
@@ -30,6 +31,12 @@ is_ordered_right(nil, _Data) -> true;
 is_ordered_right(Node, Data) ->
   is_ordered(Node) and (min(Node) >= Data).
 
+% http://ja.wikipedia.org/wiki/2分探索木
+add(#btree{left=nil , data=Data, right=Right}, D) when D =< Data -> #btree{left=#btree{data=D}, data=Data, right=Right};
+add(#btree{left=Left, data=Data, right=Right}, D) when D =< Data -> #btree{left=add(Left, D)  , data=Data, right=Right};
+
+add(#btree{left=Left, data=Data, right=nil  }, D) when D >  Data -> #btree{left=Left, data=Data, right=#btree{data=D}}; 
+add(#btree{left=Left, data=Data, right=Right}, D) when D >  Data -> #btree{left=Left, data=Data, right=add(Right, D) }.
 
 % NODE1 = btree:new(nil, 1, nil).
 % NODE2 = btree:new(nil, 2, nil).
@@ -46,3 +53,10 @@ is_ordered_right(Node, Data) ->
 % ORDERED_NODE5 = btree:new(ORDERED_NODE4, 3, ORDERED_NODE3).
 % btree:sum(ORDERED_NODE5).
 % btree:max(ORDERED_NODE5).
+
+% R1 = btree:add(ORDERED_NODE5, 10).
+% R2 = btree:add(R1, 20).
+% R3 = btree:add(R2, 15).
+% R4 = btree:add(R3, 12).
+% R5 = btree:add(R4, 14).
+% R6 = btree:add(R5, 18).
