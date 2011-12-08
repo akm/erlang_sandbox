fe0c53c8ef7f2a3d5a3265acfe9c091fbc2da1b1
akima <akima@nautilus-technologies.com>
2011-12-09 01:14:11 +0900

    execise9-3の途中ですが、一旦コミット

---
+++ chapter09/hof2.erl
@@ -0,0 +1,35 @@
+-module(hof2).
+-export([seq/1, filter_lte/1, pickup_even/1, sum/1, concat/1]).
+-export([zip/2]).
+
+seq(N) ->
+  fun() -> lists:seq(1,N) end.
+
+filter_lte(N) ->
+  Predicate = fun(X) -> X =< N end,
+  fun(List) -> lists:filter(Predicate, List) end.
+
+pickup_even(N) ->
+  Predicate = fun(X) -> X rem 2 == 0 end,
+  List = (seq(N))(),
+  fun() -> lists:filter(Predicate, List) end.
+
+sum(Numbers) ->
+  lists:foldl(fun(H, Acc) -> H + Acc end,0, Numbers).
+
+concat(Lists) ->
+  lists:foldl(fun(H, Acc) -> Acc ++ H end,[], Lists).
+
+
+% [X || X <- lists:seq(1,10), X rem 3 == 0].
+% [X * X || X <- [1, hello, 100, boo,"boo", 9], is_integer(X)].
+
+% [X || X <- [1,2,3,4,5], Y <- [4,5,6,7,8], X == Y].
+
+
+zip_acc(_ , [], Acc) -> Acc;
+zip_acc([], _ , Acc) -> Acc;
+zip_acc([XH|XT], [YH|YT], Acc) -> zip_acc(XT, YT, [{XH,YH}| Acc]).
+
+zip(Xs, Ys) -> lists:reverse(zip_acc(Xs, Ys, [])).
+
